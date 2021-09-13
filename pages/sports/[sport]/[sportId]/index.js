d53699727bbc077bcc2797bc5e0de2365c88598d
CatsAreMLG <joshuadgon@gmail.com>
2021-09-13 14:28:23 -0500

    Init

---
+++ pages/sports/[sport]/[sportId]/index.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+  const { sport, sportId } = router.query;
+  console.log(router.query)
+  return (
+    <div>
+      <h1>Router test</h1>
+      {sport}
+      {sportId}
+    </div>
+  )
+}
+export const getServerSideProps = async ({ query }) => {
+  console.log('sport index:', query);
+  return { props: {} };
+};
